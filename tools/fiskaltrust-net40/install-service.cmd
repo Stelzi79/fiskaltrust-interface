b42b5d578290efc87b1b85f4b54765e7d41bfbd2
Thomas Steininger <steininger@sysdev4u.at>
2016-07-27 02:30:10 +0200

    github-demo

---
+++ tools/fiskaltrust-net40/install-service.cmd
@@ -0,0 +1,1 @@
+fiskaltrust -cashboxid=0d1269dc-e2ae-42e3-9c57-b686d7832683  -accesstoken=BHanhRLW0WK1jyS00C+tTcJGtBHhziGWHqynd52pExpfi99QFRue+S4D/w8p5jugQr6hwJu31Parqx5256Qv9pw=  -sandbox  -i -servicename=fiskaltrust-0d1269dc-e2ae-42e3-9c57-b686d7832683
